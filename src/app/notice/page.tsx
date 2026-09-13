8e81ab5fd5a6a9da18d1f16464ed35d27463deb1
Demian <heavenin24@naver.com>
2026-09-13 15:37:31 +0900

    공지사항 제목만 상세페이지 링크로 수정

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -21,21 +21,13 @@ export default function NoticePage(){
         </thead>
         <tbody>
           {notices.map((notice)=><tr key={notice.id}>
-            <td className="num">
-              <Link className="noticeCellLink" href={`/notice/${notice.id}`} aria-label={`${notice.title} 상세보기`}>
-                {notice.id}
-              </Link>
-            </td>
+            <td className="num">{notice.id}</td>
             <td>
-              <Link className="noticeCellLink noticeTitleLink" href={`/notice/${notice.id}`}>
+              <Link className="noticeTitleLink" href={`/notice/${notice.id}`}>
                 {notice.title}
               </Link>
             </td>
-            <td className="date">
-              <Link className="noticeCellLink" href={`/notice/${notice.id}`} aria-label={`${notice.title} 상세보기`}>
-                {notice.date}
-              </Link>
-            </td>
+            <td className="date">{notice.date}</td>
           </tr>)}
         </tbody>
       </table>
